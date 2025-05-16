1eaf2a50659ceb098f1098975f5ac1df43d12a5a
ZHIQUAN LI <z.li4.24@abdn.ac.uk>
2025-05-16 19:33:57 +0000

    Fix run.py syntax error for Render deployment

--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', 'gardenframe-metermorning-5000.codio-box.uk/')
+    app.run(debug=True, host='0.0.0.0')
