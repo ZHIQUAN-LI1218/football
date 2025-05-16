cd5ea65ffd7de17de8d780a7e53c306a534a12a0
ZHIQUAN LI <z.li4.24@abdn.ac.uk>
2025-05-16 22:16:26 +0000

    Fix database

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,13 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     from .routes import main
     app.register_blueprint(main)
