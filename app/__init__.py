c223f5cfe95d31da8fdec09b724046f8b994c997
ZHIQUAN LI <z.li4.24@abdn.ac.uk>
2025-05-16 22:40:54 +0000

    third try

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from .models import db, Match, GoalScorer
+from app.extensions import db
 
 def create_app():
     app = Flask(__name__)
@@ -9,7 +9,9 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
-
+    
+    from app.models import Match, GoalScorer
+    
     with app.app_context():
         db.create_all()
 
